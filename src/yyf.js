6cf8c2d54639561a467c3c5918ab5ac38fc8386e
NewFuture <github@newfuture.cc>
2016-12-18 22:58:42 +0800

    更新调试和压缩

--- src/yyf.js
+++ src/yyf.js
@@ -1,42 +1,48 @@
-;
 (function() {
     'use strict';
+    var DEBUG = true;
+    /**
+     * default configure of http request
+     */
     var CONFIG = {
-        root: '/',
-        debug: false,
-        async: true,
-        type: 'urlencoded',
-        status: 'status',
-        data: 'info',
-        code: { //status code for status
+        root: '/', // request root url
+        async: true, // request async
+        cookie: false, // using cookie when cors site (withCredentials)
+        type: 'urlencoded', //request,Content-type,using x-www-form-urlencoded in default
+        headers: {}, //default headers in every request
+
+        status: 'status', //status field in response
+        data: 'data', // data field in response
+        _code: { //status code for status
             success: 1, //response success
             fail: 0, // response false
             auth: -1, // need auth
         },
-        codeMap: {
+        _codeMap: { //map code to status for quick search
             '1': 'success',
             '0': 'fail',
             '-1': 'auth',
         },
         setCode: function(code, status) { //set code 
             if (code) {
-                CONFIG.code[status] = code;
-                CONFIG.codeMap[code] = status;
+                CONFIG._code[status] = code;
+                CONFIG._codeMap[code] = status;
             } else {
-                delete CONFIG.code[status];
-                delete CONFIG.codeMap[code];
+                delete CONFIG._code[status];
+                delete CONFIG._codeMap[code];
             }
         },
+
         handle: { //default handlers
             auth: function() {},
             success: function() {},
-            fail: function() {}
+            fail: function() {},
+            onerror: console.error //default handle for network error
         },
-        onerror: function(res) { //default on network error
-            console.log('request error:', this, res);
-        }
     };
-    //serialize an object
+    /**
+     * serialize an object
+     */
     function serialize(obj) {
         var str = [];
         for (var p in obj)
@@ -45,131 +51,129 @@
             }
         return str.join("&");
     };
-    //http request
+    /**
+     * Http request
+     */
     var Http = function(method, async, type) {
-        this.METHOD = method || 'GET';
-        this.ASYNC = typeof async === "undefined" ? CONFIG.async : async;
-        this.TYPE = typeof type === "undefined" ? CONFIG.type : type;
-        this.HEADERS = {};
-        this.PROPS = {};
+        this._METHOD = method || 'GET';
+        this._ASYNC = typeof async === "undefined" ? CONFIG.async : async;
+        this._TYPE = typeof type === "undefined" ? CONFIG.type : type;
     }
     Http.prototype = {
-        set: function(key, value) { //set options
-            key = key.toUpperCase();
-            if (this.hasOwnProperty(key)) {
-                this[key] = value;
+        _fmt: function(data, req) { // format data and req
+            switch (this._TYPE.toLowerCase()) {
+                case 'urlencoded':
+                case 'url': //send as urlencoded form
+                    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=utf-8');
+                    if (typeof data === "string" &&
+                        data[0] === "{" && data.slice(-1) === '}') {
+                        try {
+                            data = JSON.parse(data); //json string
+                        } catch (e) {}
+                    }
+                    if (typeof data === "object") { //serialize onj
+                        return serialize(data);
+                    } else {
+                        return data;
+                    }
+                case 'json': //send as json format
+                    req.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
+                    if (typeof data === "object") {
+                        data = JSON.stringify(data);
+                    } else {
+                        return data;
+                    }
+                case 'form': //send as form // req.setRequestHeader('Content-Type', 'multipart/form-data');
+                    if (typeof data === "object") {
+                        var form = new FormData();
+                        for (var i in data) {
+                            form.append(i, d[i]);
+                        }
+                        return form
+                    } else {
+                        return data;
+                    }
+                default: //empty doesn't set Content-Type 
+                    if (this._TYPE) { //special Content type
+                        req.setRequestHeader('Content-Type', this._TYPE);
+                    }
+                    return data;
             }
-            return this;
         },
-        callback: function(on, func) { //set callback
-            if (typeof func !== "undefined") {
-                console.assert(typeof func === 'function', key + ' should be a function,', func);
-                this.PROPS[on] = func;
-            } else if (on) {
-                for (var key in on) {
-                    this.PROPS[key] = on[key];
-                }
-            }
-            return this;
-        },
-        request: function(url, data) { //send request
+        _send: function(url, data, callback) { //send request
             var req = new XMLHttpRequest();
-            for (var key in this.PROPS) { // set callback
-                if (this.PROPS[key]) {
-                    req[key] = this.PROPS[key];
+            for (var key in callback) { // set callback
+                if (callback[key]) {
+                    req[key] = callback[key];
                 }
             }
-            if (CONFIG.root && CONFIG.root !== url[0]) {
+            if (CONFIG.root !== url[0]) { // avoid root with double'/'
                 url = CONFIG.root + url;
             }
-            if (this.METHOD === 'GET' && data) {
+            if (this._METHOD === 'GET' && data) { //get serialize
                 url += '?' + serialize(data);
                 data = null;
             }
             // initialize request with URL and send
-            if (CONFIG.debug) {
+            if (DEBUG) {
                 console.log(url);
             }
-            req.open(this.METHOD, url, this.ASYNC);
-
+            req.open(this._METHOD, url, this._ASYNC);
             if (data) {
-                switch (this.TYPE.toLowerCase()) {
-                    case 'urlencoded':
-                    case 'url': //submit as urlencoded form
-                        req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=utf-8');
-                        if (typeof data === "string" && data[0] === "{" && data.slice(-1) === '}') {
-                            data = JSON.parse(data);
-                        }
-                        if (typeof data === "object") {
-                            data = serialize(data);
-                        }
-                        break;
-                    case 'json': //json format
-                        req.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
-                        if (typeof data === "object") {
-                            data = JSON.stringify(data);
-                        }
-                        break;
-                    case 'form': //submit as form
-                        // req.setRequestHeader('Content-Type', 'multipart/form-data');
-                        if (typeof data === "object") {
-                            var form = new FormData();
-                            for (var i in data) {
-                                form.append(i, d[i]);
-                            }
-                            data = form;
-                        }
-                        break;
-                    default: //empty set nothing 
-                        if (this.TYPE) { //special Content type
-                            req.setRequestHeader('Content-Type', this.TYPE);
-                        }
+                data = this._fmt(data, req);
+            }
+            for (key in CONFIG.headers) { // set headers
+                if (CONFIG.headers.hasOwnProperty(key)) {
+                    req.setRequestHeader(key, CONFIG.headers[key]);
                 }
             }
-            for (key in this.HEADERS) { // set headers
-                if (this.HEADERS.hasOwnProperty(key)) {
-                    req.setRequestHeader(key, this.HEADERS[key]);
-                }
-            }
+            req.withCredentials = CONFIG.cookie;
             req.send(data);
             return this;
         }
     };
 
+    /**
+     * yyf api
+     */
     function yyf() {
         var that = this;
-        this.onerror = CONFIG.onerror;
-        this.onload = function() { // status change
-            if (CONFIG.debug) {
+        this._onload = function() { // status change
+            if (DEBUG) {
                 console.log(this.status, this.responseText);
             }
-            if (this.readyState === XMLHttpRequest.DONE) {
+            if (this.readyState === 4) { //XMLHttpRequest.DONE
                 if (this.status >= 200 && this.status < 300) {
-                    that.handle(this.responseText, this);
+                    that._handle(this.responseText.trim(), this);
                 } else {
-                    that.onerror(this);
+                    that.getHandle('onerror');
                 }
             }
         };
-        this.handle = function(responseText, res) { //default resolve response
+        this._handle = function(responseText, res) { //default resolve response
+            if (DEBUG) {
+                console.debug(response);
+            }
+            var handler = this.getHandle('complete');
             try {
                 var response = JSON.parse(responseText);
-                var complete = this.getHandle('complete');
-                if (complete) {
-                    complete(response, res);
-                } else {
-                    var status = response[CONFIG.status];
-                    status = CONFIG.codeMap[status];
-                    var handle = this.getHandle(status);
-                    if (CONFIG.debug) {
-                        console.debug(status, handle, response);
-                    }
-                    if (handle) {
-                        handle(response[CONFIG.data]);
-                    }
+            } catch (e) { // not jso
+                if (handler) {
+                    handler(responseText);
                 }
-            } catch (e) {
-                console.error(e);
+                return;
+            }
+            if (!handler) {
+                if (CONFIG.status in response) { // get status
+                    var status = CONFIG._codeMap[response[CONFIG.status]];
+                    handler = this.getHandle(status);
+                    response = response[CONFIG.data]
+                } else { //no 'status' key in response
+                    handler = this.getHandle('onerror');
+                }
+            }
+            if (handler) {
+                handler(response, res);
             }
         };
         return this;
@@ -177,55 +181,60 @@
     yyf.prototype = {
         setHandle: function(key, callback) { //set handle for different status
             if (typeof callback !== "undefined") {
-                this.handle[key] = callback;
+                this._handle[key] = callback;
             } else if (typeof key === "function") {
-                this.handle = key;
-            } else if (CONFIG.debug) {
+                this._handle = key;
+            } else if (DEBUG) {
                 console.log("it's not callable", key, callback);
             }
             return this;
         },
         getHandle: function(status) {
             if (status) {
-                return this.handle[status] || CONFIG.handle[status];
+                return this._handle[status] || CONFIG.handle[status];
             } else {
-                return this.handle;
+                return this._handle;
             }
         },
         request: function(url, method, data, async) { //request resource
-            (new Http(method, async))
-            .callback('onload', this.onload)
-                .callback('onerror', this.onerror)
-                .request(url, data);
+            (new Http(method, async))._send(url, data, {
+                'onload': this._onload,
+                'onerror': this.getHandle('onerror')
+            });
             return this;
         },
         delete: function(url, async) {
             return this.request(url, 'DELETE', null, async);
         }
     };
-    ['get', 'put', 'post', 'patch'].forEach(function(m) {
+    ['get', 'put', 'post', 'patch'].forEach(function(m) { // method
         yyf.prototype[m] = function(url, data, async) {
             return this.request(url, m.toUpperCase(), data, async);
         };
     });
-    ['success', 'fail', 'auth', 'complete'].forEach(function(status) {
+    ['success', 'fail', 'auth', 'complete'].forEach(function(status) { //handlers
         yyf.prototype[status] = function(callback) {
             return this.setHandle(status, callback);
         }
     });
 
     /**
-     * config
+     * set config
      */
     function config(options, handle, code) {
-        if (arguments.length === 1 && (
+        if (arguments.length === 1) {
+            if (typeof options === "string") { //root
+                CONFIG['root'] = options;
+                return (new yyf());
+            } else if (
                 typeof options['options'] === "object" ||
                 typeof options['handle'] === "object" ||
                 typeof options['code'] === "object"
-            )) {
-            handle = options['handle'];
-            code = options['code'];
-            options = options['options'];
+            ) {
+                handle = options['handle'];
+                code = options['code'];
+                options = options['options'];
+            }
         }
         var key;
         for (key in options) {
@@ -245,10 +254,9 @@
      */
     var YYF = config;
     for (var func in yyf.prototype) { // yyf interfaces
-        YYF[func] = function(f) {
+        YYF[func] = function(name) {
             return function() {
-                var y = new yyf();
-                return yyf.prototype[f].apply(y, arguments);
+                return yyf.prototype[name].apply(new yyf(), arguments);
             }
         }(func);
     }
@@ -257,24 +265,16 @@
         return YYF;
     };
     YYF.getHandle = function(status) { //get default handle
-        if (status) {
-            return CONFIG.handle[status];
-        } else {
-            return CONFIG.handle;
-        }
+        return status ? CONFIG.handle[status] : CONFIG.handle;
     };
-
-    /**
-     * Vue plugin
-     */
+    // Vue plugin
     function install(Vue, options) {
         if (options) {
             config(options);
         }
         Vue.YYF = Vue.prototype.$yyf = YYF;
     }
-    if (typeof window !== 'undefined' && window.Vue) {
-        //Vue auto install
+    if (typeof window !== 'undefined' && window.Vue) { //Vue auto install
         window.Vue.use(install);
     } else {
         YYF.install = install;
