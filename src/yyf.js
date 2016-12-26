8dfbd7c48d2e7e92d1d7de2b22ae05a7fadd53d1
NewFuture <github@newfuture.cc>
2016-12-26 23:34:32 +0800

    精简变量

--- src/yyf.js
+++ src/yyf.js
@@ -142,7 +142,7 @@
     };
 
     /**
-     * yyf api
+     * yyf object
      */
     function yyf() {
         var self = this;
@@ -229,10 +229,11 @@
     });
 
     /**
+     * YYF
      * set config
      */
-    function config(options, handle, code) {
-        if (arguments.length === 1) {
+    var YYF = function(options, handle, code) {
+        if (arguments.length === 1 && options) {
             if (typeof options === 'string') { //root
                 CONFIG['root'] = options;
                 return (new yyf());
@@ -256,13 +257,10 @@
         for (key in code) {
             CONFIG._setCode(code[key], key);
         }
-        return (new yyf());
-    }
+        return YYF;
+    };
 
-    /**
-     * Expose
-     */
-    var YYF = config;
+    // apply yyf interfaces
     for (var func in yyf.prototype) { // yyf interfaces
         YYF[func] = function(name) {
             return function() {
@@ -270,31 +268,32 @@
             };
         }(func);
     }
+
+    // setCode interfaces
     YYF.setCode = function(code, status) { //set code
         CONFIG._setCode(code, status);
         return YYF;
     };
+    // get global handlers
     YYF.getHandle = function(status) { //get default handle
         return status ? CONFIG._handle[status] : CONFIG._handle;
     };
     // Vue plugin
-    function install(Vue, options) {
-        if (options) {
-            config(options);
-        }
-        Vue.YYF = Vue.prototype.$yyf = YYF;
-    }
+    YYF.install = function(Vue, options) {
+        Vue.YYF = Vue.prototype.$yyf = YYF(options);
+    };
+
+    /**
+     * Expose 
+     */
     if (typeof window !== 'undefined' && window.Vue) { //Vue auto install
-        install(window.Vue);
-    } else {
-        YYF.install = install;
-        if (typeof module !== 'undefined' && typeof exports === 'object') { //require
-            module.exports = YYF;
-        } else if (typeof define === 'function' && define.amd) { //amd
-            define(function() { return YYF; });
-        } else { //window
-            this.YYF = YYF;
-        }
+        window.Vue.use(YYF);
+    } else if (typeof module !== 'undefined' && typeof exports === 'object') { //require
+        module.exports = YYF;
+    } else if (typeof define === 'function' && define.amd) { //amd
+        define(function() { return YYF; });
+    } else { //window
+        this.YYF = YYF;
     }
 }).call(function() {
     return this || (typeof window !== 'undefined' ? window : global);
