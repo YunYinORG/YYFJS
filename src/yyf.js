23650371c27c4b8f3d5f2ab428a41c1462ff09e3
NewFuture <github@newfuture.cc>
2017-07-26 21:49:49 +0800

    添加注释

--- src/yyf.js
+++ src/yyf.js
@@ -1,8 +1,11 @@
-(function() {
+(function () {
     'use strict';
     var DEBUG = true;
+
     /**
      * global configure of http request
+     * @class CONFIG
+     * @access private
      */
     var CONFIG = {
         root: '', // request root url
@@ -23,7 +26,7 @@
             '0': 'fail',
             '-1': 'auth',
         },
-        _setCode: function(code_num, status_str) { //set code 
+        _setCode: function (code_num, status_str) { //set code 
             if (status_str) { //set
                 CONFIG._code[status_str] = code_num;
                 CONFIG._codeMap[code_num] = status_str;
@@ -35,20 +38,29 @@
 
         _handle: { //default handlers
             onerror: console.error, //default handle for network error
-            success: function() {},
-            fail: function() {}
+            success: function () { },
+            fail: function () { }
         },
     };
+
     /**
-     * serialize an object
+     * encode an object to a uri string
+     * @function serialize
+     * @param {any} obj - an object to be serialized as uri string
+     * @return {string} - seriladized uri string 
      */
     function serialize(obj) {
-        return Object.keys(obj).reduce(function(str, key) {
+        return Object.keys(obj).reduce(function (str, key) {
             return str + (str && '&') + encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]);
         }, '');
     }
+
     /**
-     * Http request
+     * @class Http
+     * @classdesc HTTP request
+     * @param {string} [method='GET'] - request method such as GET POST default is 'GET'
+     * @param {boolean} [async] - request async or sync default using CONFIG (default false)
+     * @param {string} [type] - encode type such as json,urlencoded,form; default using CONFIG (urlencode)
      */
     function Http(method, async, type) {
         this._METHOD = method || 'GET';
@@ -57,7 +69,7 @@
     }
     Http.prototype = {
         // format data and request header
-        _format: function(data, req) {
+        _format: function (data, req) {
             switch (this._TYPE.toLowerCase()) {
                 case 'urlencoded':
                 case 'url': //send as urlencoded form
@@ -92,7 +104,7 @@
             return data;
         },
         //send xhr request
-        _send: function(url, data, callback) {
+        _send: function (url, data, callback) {
             var self = this;
             var request = new XMLHttpRequest();
             var headers = CONFIG.headers;
@@ -141,12 +153,13 @@
     };
 
     /**
-     * yyf object
+     * @class yyf
+     * @classdesc REST API request
      */
     function yyf() {
         var self = this;
         // status change for http request
-        self._onload = function() {
+        self._onload = function () {
             if (DEBUG) {
                 console.log(this.status, this.responseText);
             }
@@ -159,7 +172,7 @@
             }
         };
         //default resolve response
-        self._handle = function(response, res) {
+        self._handle = function (response, res) {
             if (DEBUG) {
                 console.debug(response);
             }
@@ -189,7 +202,16 @@
         return self;
     }
     yyf.prototype = {
-        setHandle: function(key, callback) { //set handle for different status
+
+        /**
+         * set handle for different status
+         * @method
+         * @alias yyf.setHandle
+         * @param {string|number} key - status key
+         * @param {function} callback - callback on this status
+         * @return {yyf}
+         */
+        setHandle: function (key, callback) {
             if (typeof callback !== 'undefined') {
                 this._handle[key] = callback;
             } else if (typeof key === 'function') {
@@ -199,42 +221,73 @@
             }
             return this;
         },
-        getHandle: function(status) {
+
+        /**
+         * get the handle of status
+         * @method
+         * @alias yyf.getHandle
+         * @param {string|number} [status] - status key
+         * @return {function|any}
+         */
+        getHandle: function (status) {
             if (status) {
                 return this._handle[status] || CONFIG._handle[status];
             } else {
                 return this._handle;
             }
         },
-        request: function(method, url, data, async) { //request resource
+
+        /**
+         * quick request
+         * @method
+         * @alias yyf.request
+         * @param {string} method - request method such as GET POST
+         * @param {string} url - request url     
+         * @param {any} [data] - request data
+         * @param {boolean} [async] - request async or sync default using CONFIG (default false)
+         * @return {yyf}
+         */
+        request: function (method, url, data, async) { //request resource
             (new Http(method, async))._send(url, data, {
                 'onload': this._onload,
                 'onerror': this.getHandle('onerror')
             });
             return this;
         },
-        delete: function(url, async) {
+
+        /**
+        * quick delete
+        * @method
+        * @alias yyf.delete
+        * @param {string} url - request url     
+        * @param {boolean} [async] - request async or sync default using CONFIG (default false)
+        * @return {yyf}
+        */
+        delete: function (url, async) {
             return this.request('DELETE', url, null, async);
         }
     };
     // apply all REST method
-    ['get', 'put', 'post', 'patch'].forEach(function(m) {
-        yyf.prototype[m] = function(url, data, async) {
+    ['get', 'put', 'post', 'patch'].forEach(function (m) {
+        yyf.prototype[m] = function (url, data, async) {
             return this.request(m.toUpperCase(), url, data, async);
         };
     });
     //apply headers interfaces
-    ['success', 'fail', 'auth', 'ready', 'final', 'onerror'].forEach(function(status) { //handlers
-        yyf.prototype[status] = function(callback) {
+    ['success', 'fail', 'auth', 'ready', 'final', 'onerror'].forEach(function (status) { //handlers
+        yyf.prototype[status] = function (callback) {
             return this.setHandle(status, callback);
         };
     });
 
+
     /**
-     * YYF
-     * set config
+     * @class YYF
+     * @param {string|object} [options] - setting config, string for root ,obejct fpr config
+     * @param {any} [handle] - handles
+     * @param {any} [code] - code key map
      */
-    var YYF = function(options, handle, code) {
+    var YYF = function (options, handle, code) {
         if (arguments.length === 1 && options) {
             if (typeof options === 'string') { //root
                 CONFIG['root'] = options;
@@ -263,38 +316,59 @@
     };
     // apply yyf interfaces
     for (var func in yyf.prototype) { // yyf interfaces
-        YYF[func] = function(name) {
-            return function() {
+        YYF[func] = function (name) {
+            return function () {
                 return yyf.prototype[name].apply(new yyf(), arguments);
             };
         }(func);
     }
-    // setCode interfaces
-    YYF.setCode = function(code, status) { //set code
+
+    /**
+    * set global status code
+    * @method
+    * @alias YYF.setCode
+    * @param {string|number} key - status key
+    * @param {number} callback - callback on this status
+    * @return {YYF}
+    */
+    YYF.setCode = function (code, status) { //set code
         CONFIG._setCode(code, status);
         return YYF;
     };
-    // get global handlers
-    YYF.getHandle = function(status) { //get default handle
+
+    /**
+     * get the global handle of status
+     * @method
+     * @alias YYF.getHandle
+     * @param {string|number} [status] - status key
+     * @return {function|any}
+     */
+    YYF.getHandle = function (status) { //get default handle
         return status ? CONFIG._handle[status] : CONFIG._handle;
     };
+
     // Vue plugin
-    YYF.install = function(Vue, options) {
+    YYF.install = function (Vue, options) {
         Vue.YYF = Vue.prototype.$yyf = YYF(options);
     };
-
     /**
      * Expose 
      */
     if (typeof window !== 'undefined' && window.Vue) { //Vue auto install
         window.Vue.use(YYF);
     } else if (typeof module !== 'undefined' && typeof exports === 'object') { //require
+
+        /**
+         * yyfjs lib
+         * @module yyfjs
+         * @exports YYF
+         */
         module.exports = YYF;
     } else if (typeof define === 'function' && define.amd) { //amd
-        define(function() { return YYF; });
+        define(function () { return YYF; });
     } else { //window
         this.YYF = YYF;
     }
-}).call(function() {
+}).call(function () {
     return this || (typeof window !== 'undefined' ? window : global);
 }());
